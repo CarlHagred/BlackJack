17e4667dcb9dbbdaff0a3ad86dc5c83cabcb903a
CarlHagred <carl.hagred@gmail.com>
2021-11-05 23:44:10 +0100

    DB init

--- UtilitiesLib/DB/GameDbContext.cs
+++ UtilitiesLib/DB/GameDbContext.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+
 
 namespace UtilitiesLib.DB
 {
     public class GameDbContext : DbContext
     {
         public DbSet<Round> Rounds { get; set; }
-
         public DbSet<PlayerRound> PlayerRounds { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Gamedb");
     }
 }
---
+++ UtilitiesLib/Migrations/20211105223935_InitilaMigration.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UtilitiesLib.Migrations
+{
+    public partial class InitilaMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PlayerRounds",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    playerName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    score = table.Column<int>(type: "int", nullable: false),
+                    gameID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlayerRounds", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rounds",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    gameID = table.Column<int>(type: "int", nullable: false),
+                    amountOfPlayers = table.Column<int>(type: "int", nullable: false),
+                    highestScore = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rounds", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PlayerRounds");
+
+            migrationBuilder.DropTable(
+                name: "Rounds");
+        }
+    }
+}
